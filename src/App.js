4efb3fa26add6c82f3f455fc0c03a3e2cccfee3c
Robert Hendricks <rmasonh@Roberts-MacBook-Pro-4.local>
2024-02-22 18:52:42 -0500

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 //import logo from './logo.svg';
-//import './App.css';
 import React from 'react';
-import SearchSpeaker from './components/SearcSpeaker';
-//import SpeakerProfile from './components/SpeakerProfile';
-
-
-// build function speaker profile with props
-function SpeakerProfile(props) {
-  return(
-    <>
-    <h3>{props.name}</h3>
-    <p>Position: {props.jobTitle}</p>
-    <p>Company: {props.company}</p>
-    </>
-  );
+class App extends React.Component{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      name:"Andrew", }
+this.updateNameState=this.updateNameState.bind(this);
 }
-
-
-function App(){
-  return (
-    <>
-     <div style={{backgroundColor:'blueviolet', textAlign:'center',
-  color:'white'}}>
-    <h1> Speaker Profile</h1>
-    </div>
-    <div style={{backgroundColor:'midnightblue', marginTop:'0',
-  color:'white'}}>
-
-  <SpeakerProfile
-  name="Robert Hendricks"
-  jobTitle='Project Manager, Web Developer'
-  company="Bujisoft" />
-
-  <SearchSpeaker />
-  </div>
-    
-    </>
- 
-  );
+  updateNameState(){
+    this.setState({
+      name:"Andrew Peter"});
   }
-  export default App;
+render()
+  {
+    return(
+    <div>
+    <p>{this.state.name}</p>
+    <button onClick={()=>this.updateNameState()}>Display Full Name</button>
+    </div>
+      );
+  }
+}
+ export default App;
