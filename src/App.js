14e15e8db53b9fdebba4b1fa4f70616eafaadfd7
Robert Hendricks <rmasonh@Roberts-MacBook-Pro-4.local>
2024-02-21 19:13:59 -0500

    move component class to  have props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 //import logo from './logo.svg';
 //import './App.css';
 import React from 'react';
-import SpeakerProfile from './components/SpeakerProfile';
+//import SpeakerProfile from './components/SpeakerProfile';
+
+
+// build function speaker profile with props
+function SpeakerProfile(props) {
+  return(
+    <>
+    <h3>{props.name}</h3>
+    <p>Position: {props.jobTitle}</p>
+    <p>Company: {props.company}</p>
+    </>
+  );
+}
+
+
 function App(){
   return (
-  <div style={{backgroundColor: 'gray', margin:20,
+    <>
+     <div style={{backgroundColor:'blueviolet', textAlign:'center',
   color:'white'}}>
-  <SpeakerProfile />
+    <h1> Speaker Profile</h1>
+    </div>
+    <div style={{backgroundColor:'midnightblue', marginTop:'0',
+  color:'white'}}>
+
+  <SpeakerProfile
+  name="Robert Hendricks"
+  jobTitle='Project Manager, Web Developer'
+  company="Bujisoft" />
   </div>
+    
+    </>
+ 
   );
   }
   export default App;
